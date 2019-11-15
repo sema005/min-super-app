652f86495708f0cc478960c50c6aaed6c50ca6a6
Sebastian <sebastianmar00@gmail.com>
2019-11-15 14:20:01 +0100

    Updates

---
+++ precache-manifest.7642d4ef38fbfab6f465a82d496fc883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9251fca501dee210c1bbaad483efff18",
+    "url": "/min-super-app/index.html"
+  },
+  {
+    "revision": "71acc27c467f052fbc7e",
+    "url": "/min-super-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e90fd6cdb7ab722cd29b",
+    "url": "/min-super-app/static/js/2.35847c7d.chunk.js"
+  },
+  {
+    "revision": "71acc27c467f052fbc7e",
+    "url": "/min-super-app/static/js/main.1ce9e946.chunk.js"
+  },
+  {
+    "revision": "cc992419d846b39ac38c",
+    "url": "/min-super-app/static/js/runtime-main.a27b0f2e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/min-super-app/static/media/logo.25bf045c.svg"
+  }
+]);
